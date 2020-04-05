e961e2ee8847d2fd005477828faf64dda6476f5f
Ryan Sy <ryanbsy@gmail.com>
2020-04-05 03:28:44 -0400

    cors issue fix

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ var routes = require('./routes/index');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(cors());
 
 app.use('/', routes);
 
