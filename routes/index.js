f0e4e01c6b1905518975675d2fe29e93933d85cb
Ryan Sy <ryanbsy@gmail.com>
2020-04-14 14:49:07 -0400

    added square connect

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,34 @@ router.get('/search/:barcode', cors(), function(req, res) {
   });
 });
 
+router.post('/add-item', cors(), function(req, res) {
+  axios({
+    method: 'post',
+    url: 'https://connect.squareupsandbox.com/v2/catalog/object',
+    headers: {
+      'Square-Version': '2020-03-25',
+      'Authorization': `Bearer ${process.env.SQUARE_SANDBOX_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    data: {
+      'idempotency_key': '44143ec6-7de3-11ea-bc55-0242ac130003',
+      'object': {
+        'id': '#1',
+        'type': 'ITEM',
+        'item_data': {
+          'name': req.body.title,
+          'description': `UPC: ${req.body.barcode}`
+        }
+      }
+    }
+  })
+  .then(function(response) {
+    console.log('Response data:\n', response.data);
+    res.end();
+  })
+  .catch((error) => {
+    console.error('Error:\n', error.response.data);
+  })
+});
+
 module.exports = router;
