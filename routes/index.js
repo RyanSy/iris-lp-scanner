35794c2c64e16d86b86936a1c0d601670da479a6
Ryan Sy <ryanbsy@gmail.com>
2020-04-21 16:24:02 -0400

    creating item and item variation in square done

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var cors = require('cors');
 const axios = require('axios');
+const FormData = require('form-data');
+const { v4: uuidv4 } = require('uuid');
 
 router.use(cors());
 
@@ -21,6 +23,9 @@ router.get('/search/:barcode', cors(), function(req, res) {
 });
 
 router.post('/add-item', cors(), function(req, res) {
+  var catalogObjectID;
+
+  // create the item
   axios({
     method: 'post',
     url: 'https://connect.squareupsandbox.com/v2/catalog/object',
@@ -30,7 +35,7 @@ router.post('/add-item', cors(), function(req, res) {
       'Content-Type': 'application/json'
     },
     data: {
-      'idempotency_key': '44143ec6-7de3-11ea-bc55-0242ac130003',
+      'idempotency_key': uuidv4(),
       'object': {
         'id': '#1',
         'type': 'ITEM',
@@ -41,13 +46,87 @@ router.post('/add-item', cors(), function(req, res) {
       }
     }
   })
+  // create item variation
   .then(function(response) {
-    console.log('Response data:\n', response.data);
-    res.end();
+    console.log('then after creating item - Response:\n', response.data);
+    catalogObjectID = response.data.catalog_object.id;
+    console.log('catalogObjectID: ', catalogObjectID);
+    axios({
+      method: 'post',
+      url: 'https://connect.squareupsandbox.com/v2/catalog/object',
+      headers: {
+        'Square-Version': '2020-03-25',
+        'Authorization': `Bearer ${process.env.SQUARE_SANDBOX_ACCESS_TOKEN}`,
+        'Content-Type': 'application/json'
+      },
+      data: {
+        'idempotency_key': uuidv4(),
+        'object': {
+          'id': '#2',
+          'type': 'ITEM_VARIATION',
+          'item_variation_data': {
+            'name': req.body.title,
+            'item_id': catalogObjectID,
+            'price_money': {
+              'amount': parseInt(req.body.price)*100,
+              'currency': 'USD'
+            },
+            'pricing_type': 'FIXED_PRICING'
+          }
+        }
+      }
+    })
+
+    .then(function(response) {
+      console.log('then after creating item variation:\n', response.data);
+      res.end();
+    })
+    // catch create item variation error
+    .catch((error) => {
+      console.error('Error creating item variation:\n', error.response.data.errors);
+    });
   })
+
+  // // get image from discogs
+  // .then(function(response) {
+  //   console.log('then after creating item variation - Response:\n', response);
+  //   axios({
+  //     method: 'get',
+  //     url: req.body.image_url,
+  //     headers: {'User-Agent': 'IRIS-LP-Scanner/1.0'}
+  //   })
+        // catch get image from discogs error
+  //   .catch((error) => {
+  //     console.error('Error getting image from discogs:\n', error.response);
+  //   });
+  // })
+  //
+  // // save image to square
+  // .then(function(response) {
+  //   console.log('then after getting image from discogs - Response:\n', response);
+  //   const form = new FormData();
+  //   form.append('idempotency_key', Math.random().toString(36).slice(2));
+  //   form.append('object_id', catalogObjectID);
+  //   form.append('image', response);
+  //   const formHeaders = form.getHeaders();
+  //   axios.post('https://connect.squareupsandbox.com/v2/catalog/images', form, {
+  //     headers: {
+  //       ...formHeaders,
+  //       'Square-Version': '2020-03-25',
+  //       'Authorization': `Bearer ${process.env.SQUARE_SANDBOX_ACCESS_TOKEN}`
+  //     },
+  //   })
+        // catch save image to square error
+  //   .catch((error) => {
+  //     console.error('Error saving image to square:\n', error.response.data);
+  //   });
+  //   res.end();
+  // })
+
+  // catch create item error
   .catch((error) => {
-    console.error('Error:\n', error.response.data);
-  })
+    console.error('Error creating item:\n', error.response.data.errors);
+  });
 });
 
 module.exports = router;
