6f7c61728cc0c350944c273efc92eac4ba7c9251
Ryan Sy <ryanbsy@gmail.com>
2020-04-30 20:42:22 -0400

    many changes

--- routes/index.js
+++ routes/index.js
@@ -10,45 +10,91 @@ router.use(cors());
 
 require('dotenv').config();
 
+const squareRequestHeaders = {
+  'Square-Version': '2020-04-22',
+  'Authorization': `Bearer ${process.env.SQUARE_SANDBOX_ACCESS_TOKEN}`,
+  'Content-Type': 'application/json'
+};
+
+function catchError(error) {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.log(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log('Error', error.message);
+  }
+  console.log(error.config);
+}
+
 router.get('/search/:barcode', cors(), function(req, res) {
+  console.log(`/search/${req.params.barcode} route called`);
+  // search for item in catalog - if found, update price/quantity, else create new item/item variation/get image/save images
   axios({
-    method: 'get',
-    url: `https://api.discogs.com//database/search?q={${req.params.barcode}}&{?barcode}&token=${process.env.DISCOGS_USER_TOKEN}`,
-    headers: {'User-Agent': 'IRIS-LP-Scanner/1.0'}
-  })
-  .then(function(response) {
-    console.log(response.data.results[0]);
-    res.json(response.data.results[0]);
-  })
-  .catch(function (error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message);
-    }
-    console.log(error.config);
-  });
+          method: 'post',
+          url: 'https://connect.squareupsandbox.com/v2/catalog/search',
+          headers: squareRequestHeaders,
+          data: {
+            'object_types': ['ITEM_VARIATION'],
+            'query': {
+              'text_query': {
+                'keywords': [req.params.barcode]
+              }
+            }
+          }
+      })
+      .then(function(response) {
+        if (response.data.objects) {
+          console.log('search square result:\n', response.data.objects[0]);
+          // retrieve inventory count
+          res.json(response.data.objects[0]);
+        } else {
+          console.log('item not found in catalog, searching discogs');
+          searchDiscogs();
+          }
+      })
+      .catch(function(error) {
+        console.log('error searching square');
+        catchError(error);
+      });
+
+  function retrieveInventory() {
+
+  }
+
+  function searchDiscogs() {
+    console.log('searchDiscogs() called');
+    return axios({
+      method: 'get',
+      url: `https://api.discogs.com//database/search?q={${req.params.barcode}}&{?barcode}&token=${process.env.DISCOGS_USER_TOKEN}`,
+      headers: {'User-Agent': 'IRIS-LP-Scanner/1.0'}
+    })
+    .then(function(response) {
+      if (response.data.results.length > 0) {
+        console.log('item found on discogs:\n', response.data.results[0]);
+        res.json(response.data.results[0]);
+      } else {
+        console.log('item not found on discogs');
+        res.json({ title: 'Item not found' });
+      }
+    })
+    .catch(function(error) {
+      console.log('error searching discogs');
+      catchError(error);
+    });
+  }
 });
 
 router.post('/add-item', cors(), function(req, res) {
-  // search for item in catalog - if found, update price/quantity, else create new item/item variation/get image/save images
-
-  const squareRequestHeaders = {
-    'Square-Version': '2020-04-22',
-    'Authorization': `Bearer ${process.env.SQUARE_SANDBOX_ACCESS_TOKEN}`,
-    'Content-Type': 'application/json'
-  };
-
+  console.log('/add-item route called');
   function createItem() {
     return axios({
             method: 'post',
@@ -72,24 +118,8 @@ router.post('/add-item', cors(), function(req, res) {
             var catalogObjectID = response.data.catalog_object.id;
             createItemVariation(catalogObjectID);
           })
-          .catch(function (error) {
-            console.log('========== error creating item ==========');
-            if (error.response) {
-              // The request was made and the server responded with a status code
-              // that falls out of the range of 2xx
-              console.log(error.response.data);
-              console.log(error.response.status);
-              console.log(error.response.headers);
-            } else if (error.request) {
-              // The request was made but no response was received
-              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-              // http.ClientRequest in node.js
-              console.log(error.request);
-            } else {
-              // Something happened in setting up the request that triggered an Error
-              console.log('Error', error.message);
-            }
-            console.log(error.config);
+          .catch(function(error) {
+            catchError(error);
           });
   }
 
@@ -107,7 +137,7 @@ router.post('/add-item', cors(), function(req, res) {
                   'item_id': catalogObjectID,
                   'name': req.body.title,
                   'price_money': {
-                    'amount': parseInt(req.body.price)*100,
+                    'amount': parseFloat(req.body.price)*100,
                     'currency': 'USD'
                   },
                   'pricing_type': 'FIXED_PRICING',
@@ -123,23 +153,7 @@ router.post('/add-item', cors(), function(req, res) {
             changeQuantity(itemVariationID);
           })
           .catch(function (error) {
-            console.log('========== error creating item variation ==========');
-            if (error.response) {
-              // The request was made and the server responded with a status code
-              // that falls out of the range of 2xx
-              console.log(error.response.data);
-              console.log(error.response.status);
-              console.log(error.response.headers);
-            } else if (error.request) {
-              // The request was made but no response was received
-              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-              // http.ClientRequest in node.js
-              console.log(error.request);
-            } else {
-              // Something happened in setting up the request that triggered an Error
-              console.log('Error', error.message);
-            }
-            console.log(error.config);
+            catchError(error);
           });
   }
 
@@ -173,27 +187,11 @@ router.post('/add-item', cors(), function(req, res) {
             console.log(response.data);
           })
           .catch(function (error) {
-            console.log('========== error changing quantity ==========');
-            if (error.response) {
-              // The request was made and the server responded with a status code
-              // that falls out of the range of 2xx
-              console.log(error.response.data);
-              console.log(error.response.status);
-              console.log(error.response.headers);
-            } else if (error.request) {
-              // The request was made but no response was received
-              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-              // http.ClientRequest in node.js
-              console.log(error.request);
-            } else {
-              // Something happened in setting up the request that triggered an Error
-              console.log('Error', error.message);
-            }
-            console.log(error.config);
+            catchError(error);
           });
   }
 
-  function getImage() {
+  function createImage() {
     axios({
       method: 'get',
       url: req.body.image_url,
@@ -201,96 +199,21 @@ router.post('/add-item', cors(), function(req, res) {
       headers: {'User-Agent': 'IRIS-LP-Scanner/1.0'}
     })
     .then(function(response) {
-      console.log(`========== got image ==========\n${response.data}`);
+      console.log(`========== got image ==========\n ${response.data}`);
       response.data.pipe(fs.createWriteStream(`./tmp/${req.body.title}.jpg`));
     })
     .catch(function (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
+      catchError(error);
     });
   }
 
-  axios.all([createItem(), getImage()])
+  axios.all([createItem(), createImage()])
     .then(axios.spread(function(one, two) {
       res.end();
     }))
     .catch(function (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
+      catchError(error);
     });
-
-
-
-  // save image to square
-    // .then(function(response) {
-    //   response.data.pipe(fs.createWriteStream(`./tmp/${req.body.title}.jpg`));
-    //     const form = new FormData();
-    //     form.append('idempotency_key', uuidv4());
-    //     form.append('object_id', catalogObjectID);
-    //     form.append('image', fs.createReadStream(`./tmp/${req.body.title}.jpg`));
-    //     const formHeaders = form.getHeaders();
-    //     axios.post('https://connect.squareupsandbox.com/v2/catalog/images', form, {
-    //       headers: {
-    //         ...formHeaders,
-    //         'Accept': 'application/json',
-    //         'Square-Version': '2020-03-25',
-    //         'Authorization': `Bearer ${process.env.SQUARE_SANDBOX_ACCESS_TOKEN}`,
-    //         'Content-Type': 'multipart/form-data'
-    //       },
-    //     })
-  //         // catch save image to square error
-  //         .catch((error) => {
-  //           console.error('Error saving image to square:\n', error.response.data);
-  //         });
-  //         res.end();
-  //     })
-  //     // catch get image from discogs error
-  //     .catch((error) => {
-  //       console.error('Error getting image from discogs:\n', error);
-  //     });
-  //   })
-  //   // catch create item variation error
-  //   .catch((error) => {
-  //     console.error('Error creating item variation:\n', error.response.data.errors);
-  //   });
-  // })
-
-  //
-  // // save image to square
-  // .then(function(response) {
-  //   console.log('then after getting image from discogs - Response:\n', response);
-
-  // })
-
-  // catch create item error
 
 }); // end add-item route
 
