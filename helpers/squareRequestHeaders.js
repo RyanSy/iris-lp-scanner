db8bbac9f6d42b46726e392c5df2e593acb9d8b9
Ryan Sy <ryanbsy@gmail.com>
2020-06-09 02:00:58 -0400

    cors fix

--- helpers/squareRequestHeaders.js
+++ helpers/squareRequestHeaders.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
   'Square-Version': '2020-05-28',
   'Authorization': `Bearer ${process.env.SQUARE_ACCESS_TOKEN}`,
-  'Content-Type': 'application/json'
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json',
 };
