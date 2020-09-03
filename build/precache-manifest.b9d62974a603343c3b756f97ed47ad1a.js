a22432f9a2991e49553392f677cf86e1ad1e2f93
Ryan Sy <ryanbsy@gmail.com>
2020-09-03 18:17:20 -0400

    fixed update item bug

---
+++ build/precache-manifest.b9d62974a603343c3b756f97ed47ad1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83fbc8081d696e01391d64f40ff2aaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af4d948535c2d4f6b50e",
+    "url": "/static/js/2.a4f6ab3c.chunk.js"
+  },
+  {
+    "revision": "ec514801e73eabbba6e16da32a2e82cd",
+    "url": "/static/js/2.a4f6ab3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ad17ac8aead2781e2d",
+    "url": "/static/js/main.69f5e06b.chunk.js"
+  },
+  {
+    "revision": "1dc80c2ca5a292ffc1df",
+    "url": "/static/js/runtime-main.20abe852.js"
+  },
+  {
+    "revision": "0f067fa599bc1be7664ded769927f694",
+    "url": "/static/media/recordSpinning.0f067fa5.mp4"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6dc1fc0064683d4054ffacfc08328ca7.js"
+  "/precache-manifest.b9d62974a603343c3b756f97ed47ad1a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
