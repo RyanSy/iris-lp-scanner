768162d5a51a7d9ead11d8db17f6a2ff3e03d80c
Ryan Sy <ryanbsy@gmail.com>
2021-01-07 06:17:06 -0500

    a little pruning

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var cors = require('cors');
 var app = express();
 var searchController = require('./controllers/searchController');
 var createController = require('./controllers/createController');
 var updateController = require('./controllers/updateController');
 var port = process.env.PORT || 8080;
 
-app.use(cors());
-app.options('*', cors());
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 
 app.get('/search/:barcode', searchController);
 
